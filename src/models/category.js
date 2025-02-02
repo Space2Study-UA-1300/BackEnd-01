e88c0f359f13882847b2f905cb73606bbfa7be4e
tetianasvidrak <svidrak.tetiana@gmail.com>
2025-02-02 12:29:21 +0100

    created category model

---
+++ src/models/category.js
@@ -0,0 +1,20 @@
+const { Schema, model } = require('mongoose')
+
+const { FIELD_CANNOT_BE_EMPTY } = require('~/consts/errors')
+const { CATEGORY } = require('~/consts/models')
+
+const categorySchema = new Schema({
+  name: {
+    type: String,
+    required: [true, FIELD_CANNOT_BE_EMPTY('name')],
+    unique: true
+  },
+  appearance: {
+    type: {
+      icon: { type: String, default: 'assets/img/categories/default-icon.svg' },
+      color: { type: String, default: '#66C42C' }
+    }
+  }
+})
+
+module.exports = model(CATEGORY, categorySchema)
